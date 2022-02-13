ab4da4fdcb78fed8866ecf7d3b036018f33dc83d
anjali-2003 <96301802+anjali-2003@users.noreply.github.com>
2022-02-13 17:22:52 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,87 @@
+
+var gamePattern = [];
+
+var userClickedPattern = [];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var start = false;
+
+var level = 0;
+
+var currentLevel;
+
+$(".btn").click( function(){
+
+	if(!start){
+		start=true;
+		$("#level-title").text("Level "+level);
+		nextSequence();
+
+	}
+
+	var userChosenColour = $(this).attr("id");
+	userClickedPattern.push(userChosenColour);
+	playSound(userChosenColour);
+	animatePress(userChosenColour);
+	checkAnswer(userClickedPattern.length-1);
+});
+
+// $(document).keypress(function(){
+// 	if(!start){
+// 		start=true;
+// 		$("#level-title").text("Level "+level);
+// 		nextSequence();
+//
+// 	}
+// });
+
+function nextSequence(){
+	  userClickedPattern=[];
+    level++;
+    $("#level-title").text("Level "+level);
+
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+		gamePattern.push(randomChosenColour);
+		$("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(userChosenColour);
+}
+
+function playSound(name){
+	var audio = new Audio("sounds/"+name+".mp3");
+	audio.play();
+}
+
+function animatePress(currentColour){
+	$("."+currentColour).addClass("pressed");
+	setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel){
+	if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+		console.log("success");
+		if (userClickedPattern.length === gamePattern.length){
+			setTimeout(function () {
+				nextSequence();
+			}, 1000);
+		}
+	}
+	else{
+		playSound("wrong");
+		$("body").addClass("game-over");
+		setTimeout(function(){
+			$("body").removeClass("game-over");
+		},200 );
+		$("h1").text("Game Over! Press Any Button to Restart");
+		startOver();
+	}
+}
+
+function startOver(){
+	level = 0;
+	start = false;
+	gamePattern = [];
+}
